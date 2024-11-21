d6a782a9cd83555640e77f4af2501da69de36019
Samuel Sukovský <145551742+SamuelSukovsky@users.noreply.github.com>
2024-11-21 10:26:26 +0000

    Empty functions and test class
    Added empty functions to the treemap class
    Created the TreemapTest class

--- CA2 project/TreeMap.h
+++ CA2 project/TreeMap.h
@@ -1,3 +1,66 @@
 #pragma once
 #include "BinaryTree.h"
 
+template <class K, class V>
+class TreeMap
+{
+	BinaryTree<TreemapNode<K, V>> tree;
+public:
+	void clear();
+	bool containsKey(K key);
+	V& get(K key);
+	BinaryTree<K> keySet();
+	void put(K key, V value);
+	int size();
+	bool removeKey(K key);
+	V& operator[K key];
+
+	~TreeMap();
+};
+
+template <class K, class V>
+void TreeMap<K, V>::clear()
+{
+	tree.clear();
+}
+
+template <class K, class V>
+bool TreeMap<K, V>::containsKey(K key)
+{
+	return false;
+}
+
+template <class K, class V>
+V& TreeMap<K, V>::get(K key)
+{
+	return null;
+}
+
+template <class K, class V>
+BinaryTree<K> TreeMap<K, V>::keySet()
+{
+	return null;
+}
+
+template <class K, class V>
+void TreeMap<K, V>::put(K key, V value)
+{
+}
+
+template <class K, class V>
+int TreeMap<K, V>::size()
+{
+	return 0;
+}
+
+template <class K, class V>
+bool TreeMap<K, V>::removeKey(K key)
+{
+	return false;
+}
+
+template <class K, class V>
+TreeMap<K, V>::~TreeMap()
+{
+	delete tree;
+}
---
+++ CA2 project/TreemapNode.h
@@ -0,0 +1,10 @@
+#pragma once
+
+using namespace std;
+template <class K, class U>
+class TreemapNode
+{
+public:
+	K key;
+	U value;
+};
